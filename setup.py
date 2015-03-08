cf77f52076a8596153e38f1dfe7891448ad6feb5
jkr <joakim.karlsson@lundalogik.se>
2015-03-08 11:14:11 +0100

    64-bit version builds

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import subprocess as sp
 import argparse as ap
 import os.path as op
 import shutil
+import glob
 import sys
 import os
 
@@ -13,6 +14,15 @@ def main():
     ensure_mercurial()
     ensure_source_available()
     pull_source_if(args.pull)
+
+    env = visual_studio_environment()
+    add_python_path_to(env)
+    add_python_build_config_to(env)
+    clean(env)
+    build(env)
+
+    ensure_bin_dir()
+    copy_binaries()
 
 
 def ensure_mercurial():
@@ -37,6 +47,65 @@ def pull_source_if(should_pull):
     hg_pull()
 
 
+def copy_binaries():
+    print('Copying runtime files...')
+    shutil.copytree(runtime_dir(), bin_dir())
+    for f in binaries():
+        print('Copying {}...'.format(f))
+        shutil.copy(f, bin_dir())
+
+
+def binaries():
+    return (glob.glob(op.join(vim_src_dir(), '**/*.exe')) +
+            glob.glob(op.join(vim_src_dir(), '*.exe')) +
+            glob.glob(op.join(vim_src_dir(), '**/*.dll')) +
+            glob.glob(op.join(vim_src_dir(), '*.dll')))
+
+
+
+
+def visual_studio_environment():
+    res = sp.check_output(['cmd', '/c', 'vsvarshelper.bat'],
+                          cwd=script_dir())
+    res = res.decode('utf-8')
+
+    return dict(line.split('=') for line in res.split('\r\n') if '=' in line)
+
+def add_python_build_config_to(env):
+    # env['GUI'] = 'yes'
+    # env['DIRECTX'] = 'yes'
+    env['CPUNR'] = 'i686'
+    env['CPU'] = 'AMD64'
+    env['FEATURES'] = 'huge'
+    env['FARSI'] = 'no'
+    env['ARABIC'] = 'no'
+    env['DYNAMIC_PYTHON3'] = 'yes'
+    env['PYTHON3'] = r'C:\Python34'
+    env['PYTHON3_VER'] = '34'
+
+
+def add_python_path_to(env):
+    env['PATH'] = r'C:\Python34;' + env['PATH']
+
+
+def build(env):
+    print('Building vim...')
+    sp.check_call(['cmd', '/c', nmake(), '-f', 'Make_mvc.mak', 'CPU=AMD64'], env=env, cwd=vim_src_dir())
+
+
+def clean(env):
+    print('Cleaning up build...')
+    sp.check_call(['cmd', '/c', nmake(), '-f', 'Make_mvc.mak', 'clean'], env=env, cwd=vim_src_dir())
+
+
+def nmake():
+    return op.join(vc_bin_dir(), 'nmake')
+
+def vc_bin_dir():
+    return op.expandvars(op.join(
+        '%PROGRAMFILES(X86)%', 'Microsoft Visual Studio 10.0', 'VC', 'bin'))
+
+
 def hg_clone():
     sp.check_call(['hg', 'clone', 'https://vim.googlecode.com/hg/', vim_dir()])
 
@@ -48,6 +117,24 @@ def hg_pull():
 
 def vim_source_available():
     return op.isdir(vim_dir())
+
+
+def ensure_bin_dir():
+    print('Ensuring bin dir is available...')
+    if op.isdir(bin_dir()):
+        print('Removing old bin dir...')
+        shutil.rmtree(bin_dir())
+
+
+def bin_dir():
+    return op.join(script_dir(), 'bin')
+
+def runtime_dir():
+    return op.join(vim_dir(), 'runtime')
+
+
+def vim_src_dir():
+    return op.join(vim_dir(), 'src')
 
 
 def vim_dir():
---
+++ vsvarshelper.bat
@@ -0,0 +1,3 @@
+@echo off
+call "%PROGRAMFILES(X86)%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86_amd64
+set
